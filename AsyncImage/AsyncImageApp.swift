3dd9991e1da895003bc81bdae4f5d17d37fee2ce
Debotos Das <debotosdas@gmail.com>
2023-07-27 08:34:07 +0600

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Personal on 27/7/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
